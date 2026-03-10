6636aa0cdbd281e4557a89c2f1645ad9834b61b1
drworman <david@worman.com>
2026-03-10 13:53:08 -0500

    20260310b

--- core/state.py
+++ core/state.py
@@ -18,7 +18,7 @@ from pathlib import Path
 PROGRAM = "Elite Dangerous Monitor Daemon"
 DESC    = "Continuous monitoring of Elite Dangerous AFK sessions."
 AUTHOR  = "CMDR CALURSUS"
-VERSION = "20260310a"
+VERSION = "20260310b"
 GITHUB_REPO = "drworman/EDMD"
 DEBUG_MODE  = False
 
