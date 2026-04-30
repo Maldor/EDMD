1c01cccd12f7496598a0766bad260baf9e320b49
Maldor <drahric.techy@gmail.com>
2026-04-29 21:58:29 -0600

    Update Data.py CAPI config to use a new Client_ID since the old one is now in an unknown state.

--- core/data.py
+++ core/data.py
@@ -110,7 +110,8 @@ RING_SIZE = 200   # max events stored per event type
 
 CAPI_BASE    = "https://companion.orerve.net"
 AUTH_BASE    = "https://auth.frontierstore.net"
-CLIENT_ID    = "25ae274d-d16b-45e5-bbf3-d143a401d1a7"
+CLIENT_ID    = "0a0bfa8e-18e7-4027-ab50-a697a2d5b2cd" # 25ae274d-d16b-45e5-bbf3-d143a401d1a7
+# Mine is in there, but the original devs ID is still there for legacy purposes
 REDIRECT_URI = "https://maldor.github.io/EDMD/auth/callback"
 SCOPE        = "auth capi"
 
